baaf90bfd9d1d21c6d35816223480233c39150b3
hmingji <hmingji@hotmail.com>
2023-02-13 16:28:30 +0800

    adjust footer zIndex

--- client/components/common/Footer.tsx
+++ client/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="container mx-auto px-2 py-16 justify-center flex flex-col gap-2 z-30">
-      <p className="text-lg text-gray-500 text-center">
+    <footer className="container mx-auto px-2 py-16 justify-center flex flex-col gap-2">
+      <p className="text-lg text-gray-500 text-center z-20">
         Data Source:{' '}
         <a
           href="https://github.com/MoH-Malaysia/data-darah-public"
@@ -11,7 +11,7 @@ export default function Footer() {
         </a>
       </p>
 
-      <p className="text-lg text-gray-500 text-center">
+      <p className="text-lg text-gray-500 text-center z-20">
         <a
           href="https://github.com/hmingji/DataViz"
           className="text-blue-500 underline"
@@ -20,7 +20,7 @@ export default function Footer() {
         </a>
       </p>
 
-      <p className="text-lg text-gray-500 text-center">
+      <p className="text-lg text-gray-500 text-center z-20">
         By{' '}
         <a
           href="https://github.com/hmingji"
